80a8f64cf60230f92500c7ada3c7f6cfd94451d4
Jack Fisher <jackfisher2012@gmail.com>
2018-04-12 13:17:23 +0100

    Refactor package name

--- src/main/scala/com/jd/nameidentify/package.scala
+++ src/main/scala/com/jd/nameidentify/package.scala
@@ -3,7 +3,7 @@ package com.jd
 /**
   * Provides classes for creating secrets and revealing them
   */
-package object NameIdenifier {
+package object nameidentify {
   /**
     * ==Overview==
     * The main classes are [com.jd.nameidentifier.LatinGen] and [com.jd.nameidentifier.SecretGen,]
